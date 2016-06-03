df313c04b6d9d5030512cff55ad792c8170bdc89
oliverheilig <oliver.heilig@yahoo.de>
2016-06-03 15:42:43 +0200

    added onevstwo

--- token.js
+++ token.js
@@ -1,3 +1,3 @@
 // The token below is for internal use only - consider getting your own token!
 // This token is valid until 2015-10-09T18:03:29Z and was auto-generated @ 2015-08-04T21:51:32Z.
-var token = '11333604106012';
+var token = '953B0471-1EB8-4E1C-B170-ACDF1B04D6B5';
