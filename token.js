d2bab5309f5ae2f17df69d2b90d7823fc7ed6431
oliverheilig <oliver.heilig@yahoo.de>
2016-08-02 10:27:38 +0200

    using cdnjs for leaflet

--- token.js
+++ token.js
@@ -1,3 +1,2 @@
-// The token below is for internal use only - consider getting your own token!
-// This token is valid until 2015-10-09T18:03:29Z and was auto-generated @ 2015-08-04T21:51:32Z.
-var token = '953B0471-1EB8-4E1C-B170-ACDF1B04D6B5';
+// The token below is for internal use only - use your own token!
+var token = '10806EE8-1808-4069-A281-70EA1692CE18';
