de6991613b40e9d1261cb75174bc9ad009fa9806
oliverheilig <oliver.heilig@yahoo.de>
2016-08-19 12:07:47 +0200

    fixed geocoding

--- lrm-xserver2/Control.Geocoder-xs2.ptv.js
+++ lrm-xserver2/Control.Geocoder-xs2.ptv.js
@@ -9,7 +9,7 @@ L.Control.Geocoder.Ptv = L.Class.extend({
 	initialize: function (options) {
 		L.Util.setOptions(this, options);
 	},
-
+	
 	runRequest: function (url, request, token, handleSuccess, handleError) {
 		$.ajax({
 			url: url + request.address + '?xtok=' + token,
@@ -68,8 +68,76 @@ L.Control.Geocoder.Ptv = L.Class.extend({
 		);
 	},
 
-	reverse: function (query, cb, context) {
-		//NOT IMPLEMENTED
+	// use xlocate1 for reverse geocoding
+	
+	runPostRequest: function (url, request, token, handleSuccess, handleError) {
+		$.ajax({
+			url: url,
+			type: 'POST',
+			data: JSON.stringify(request),
+
+			headers: {
+				'Authorization': 'Basic ' + btoa('xtok:' + token),
+				'Content-Type': 'application/json'
+			},
+
+			success: function (data, status, xhr) {
+				handleSuccess(data);
+			},
+
+			error: function (xhr, status, error) {
+				handleError(xhr);
+			}
+		});
+	},
+
+	reverse: function (location, scale, cb, context) {
+		var url = 'https://api-test.cloud.ptvgroup.com/xlocate/rs/XLocate/findLocation';
+
+		var request = {
+			location: {
+				coordinate: {
+					point: {
+						x: location.lng,
+						y: location.lat
+					}
+				}
+			},
+			options: [],
+			sorting: [],
+			additionalFields: [],
+			callerContext: {
+				properties: [
+				{
+					key: 'CoordFormat',
+					value: 'OG_GEODECIMAL'
+				},
+				{
+					key: 'Profile',
+					value: 'default'
+				}]
+			}
+		};
+
+		this.runPostRequest(url, request, this.options.token,
+			L.bind(function (response) {
+				if (response.resultList.length === 0) {
+					return;
+				}
+
+				var resultAddress = response.resultList[0];
+				var loc = L.latLng(resultAddress.coordinates.point.y, resultAddress.coordinates.point.x);
+				cb.call(context, [{
+					name: this._buildAddressString(resultAddress),
+					center: loc,
+					bounds: L.latLngBounds(loc, loc)
+				}]);
+			}, this),
+
+			function (xhr) {
+				console.log(xhr);
+			}
+		);
 	}
 });
 
