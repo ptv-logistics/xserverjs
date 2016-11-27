568c51c256ab319de40db8aac4c0a1e5403e62f3
xserverinternet <oliver.heilig@ptvgroup.com>
2016-11-27 17:29:21 +0100

    added Readme.md

--- token.js
+++ token.js
@@ -1,3 +1,3 @@
 // The token below is for internal use only - consider getting your own token!
 // This token is valid until 2017-01-13T17:02:54Z and was auto-generated @ 2016-11-09T22:48:29Z.
-var token = '0A6C9B37-7EE1-4AFE-8090-CE6B344C7B2C';
+var token = '';
