5cc908e761860e047346f8dc46e5be3d6da26244
oliver.heilig@yahoo.de <oliver.heilig@yahoo.de>
2017-01-18 14:07:16 +0100

    updates for master

--- token.js
+++ token.js
@@ -1,3 +1,2 @@
-// The token below is for internal use only - consider getting your own token!
-// This token is valid until 2017-04-07T17:03:11Z and was auto-generated @ 2017-01-17T22:16:53Z.
+// add your xserver-internet token here
 var token = '';
