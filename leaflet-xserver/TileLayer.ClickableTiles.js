2f6bf9786a5241dc2f2346821cd901bb08c6025b
oliver.heilig@yahoo.de <oliver.heilig@yahoo.de>
2017-01-23 17:59:35 +0100

    updated build

--- leaflet-xserver/TileLayer.ClickableTiles.js
+++ leaflet-xserver/TileLayer.ClickableTiles.js
@@ -1,4 +1,9 @@
-﻿L.TileLayer.ClickableTiles = L.TileLayer.extend({
+﻿"use strict";
+
+var L = require('leaflet'),
+    corslite = require('corslite');
+
+L.TileLayer.ClickableTiles = L.TileLayer.extend({
     includes: L.Mixin.Events,
 
     initialize: function (url, options) {
@@ -14,7 +19,6 @@
     
         cont.addEventListener('mousemove', L.bind(this._onMouseMove, this), true);
         cont.addEventListener('mousedown', L.bind(this._onMouseDown, this), true);
-        // cont.addEventListener('mouseup', L.bind(this._onMouseDown, this), true);
     
         map._mapPane.addEventListener('click', L.bind(this._onClick, this), true);
         map.addEventListener('click', L.bind(this._onMapClick, this), false);
@@ -27,7 +31,6 @@
     
         cont.removeEventListener('mousemove', L.bind(this._onMouseMove, this), true);
         cont.removeEventListener('mousedown', L.bind(this._onMouseDown, this), true);
-        // cont.removeEventListener('mouseup', L.bind(this._onMouseDown, this), true);
     
         map._mapPane.removeEventListener('click', L.bind(this._onClick, this), true);
         map.removeEventListener('click', L.bind(this._onMapClick, this), false);
@@ -249,7 +252,7 @@
 		*/
         tile.setAttribute('role', 'presentation');
 
-        url = this.getTileUrl(coords);
+        var url = this.getTileUrl(coords);
 
         tile._map = this._map;
         tile._layers = [];
@@ -295,96 +298,4 @@ L.TileLayer.clickableTiles = function (url, options) {
     return new L.TileLayer.ClickableTiles(url, options);
 };
 
-function corslite(url, callback, cors) {
-    var sent = false;
-
-    if (typeof window.XMLHttpRequest === 'undefined') {
-        return callback(Error('Browser not supported'));
-    }
-
-    if (typeof cors === 'undefined') {
-        var m = url.match(/^\s*https?:\/\/[^\/]*/);
-        cors = m && (m[0] !== location.protocol + '//' + location.hostname +
-            (location.port ? ':' + location.port : ''));
-    }
-
-    var x = new window.XMLHttpRequest();
-
-    function isSuccessful(status) {
-        return status >= 200 && status < 300 || status === 304;
-    }
-
-    if (cors && !('withCredentials' in x)) {
-        // IE8-9
-        x = new window.XDomainRequest();
-
-        // Ensure callback is never called synchronously, i.e., before
-        // x.send() returns (this has been observed in the wild).
-        // See https://github.com/mapbox/mapbox.js/issues/472
-        var original = callback;
-        callback = function () {
-            if (sent) {
-                original.apply(this, arguments);
-            } else {
-                var that = this, args = arguments;
-                setTimeout(function () {
-                    original.apply(that, args);
-                }, 0);
-            }
-        }
-    }
-
-    function loaded() {
-        if (
-            // XDomainRequest
-            x.status === undefined ||
-            // modern browsers
-            isSuccessful(x.status)) callback.call(x, null, x);
-        else callback.call(x, x, null);
-    }
-
-    // Both `onreadystatechange` and `onload` can fire. `onreadystatechange`
-    // has [been supported for longer](http://stackoverflow.com/a/9181508/229001).
-    if ('onload' in x) {
-        x.onload = loaded;
-    } else {
-        x.onreadystatechange = function readystate() {
-            if (x.readyState === 4) {
-                loaded();
-            }
-        };
-    }
-
-    // Call the callback with the XMLHttpRequest object as an error and prevent
-    // it from ever being called again by reassigning it to `noop`
-    x.onerror = function error(evt) {
-        // XDomainRequest provides no evt parameter
-        callback.call(this, evt || true, null);
-        callback = function () { };
-    };
-
-    // IE9 must have onprogress be set to a unique function.
-    x.onprogress = function () { };
-
-    x.ontimeout = function (evt) {
-        callback.call(this, evt, null);
-        callback = function () { };
-    };
-
-    x.onabort = function (evt) {
-        callback.call(this, evt, null);
-        callback = function () { };
-    };
-
-    // GET is the only supported HTTP Verb by XDomainRequest and is the
-    // only one supported here.
-    x.open('GET', url, true);
-
-    // Send the request. Sending data is not supported.
-    x.send(null);
-    sent = true;
-
-    return x;
-}
-
-if (typeof module !== 'undefined') module.exports = corslite;
+module.exports = L.TileLayer.ClickableTiles;
