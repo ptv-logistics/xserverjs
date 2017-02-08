d80de947be4a9bc789bdb356cfbc1d6edc96ece8
oliverheilig <oliver.heilig@yahoo.de>
2017-02-08 23:02:47 +0100

    updated apis

--- token.js
+++ token.js
@@ -1,3 +1,2 @@
-// The token below is for internal use only - consider getting your own token!
-// This token is valid until 2017-04-07T17:03:11Z and was auto-generated @ 2017-02-07T22:17:14Z.
-var token = 'FE44E783-0708-4A44-A71C-99B25008C940';
+// add your xserver-internet token here
+var token = '';
