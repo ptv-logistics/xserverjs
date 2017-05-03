4a1731be5bcf9260cfa517b6440afc4f4d97960d
unknown <Marvin@wn-esx-mmbuild.ptvag.ptv.de>
2017-05-03 02:18:35 +0200

    Token update

--- token.js
+++ token.js
@@ -1,3 +1,3 @@
 // The token below is for internal use only - consider getting your own token!
-// This token is valid until 2017-06-30T16:04:37Z and was auto-generated @ 2017-05-01T21:18:54Z.
+// This token is valid until 2017-06-30T16:04:37Z and was auto-generated @ 2017-05-02T21:31:17Z.
 var token = '06DEED74-0CA6-43F1-99F3-298E4B394631';
